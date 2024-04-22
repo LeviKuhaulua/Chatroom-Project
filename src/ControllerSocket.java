0235eb1e34ea099939ea6443757a63bccb932dcf
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-04-22 09:23:08 -1000

    Controller Socket file created. Will get relevant information from Clients then send them to each other so that they can establish communication with each other.

---
+++ src/ControllerSocket.java
@@ -0,0 +1,45 @@
+import java.net.ServerSocket; 
+import java.net.Socket; 
+import java.io.PrintWriter; 
+import java.io.IOException;
+public class ControllerSocket {
+    public static void main(String[] args) {
+        
+
+        try (ServerSocket connect = new ServerSocket(12345)) {
+
+            Socket client1 = connect.accept(); 
+            Socket client2 = connect.accept(); 
+
+            // Get the IP and port number of both clients
+            String client1IP = client1.getInetAddress().getHostAddress(); 
+            int client1Port = client1.getLocalPort();  
+
+            String client2IP = client2.getInetAddress().getHostAddress(); 
+            int client2Port = client2.getLocalPort(); 
+
+
+            // Send the IP and Local Port information to both clients. 
+            PrintWriter toClient1 = new PrintWriter(client1.getOutputStream(), true); 
+            PrintWriter toClient2 = new PrintWriter(client2.getOutputStream(), true); 
+
+            // Pattern -: IP Address then the Port Number associated with the client. 
+            toClient1.println(client2IP); 
+            toClient1.println(client2Port); 
+
+            toClient2.println(client1IP); 
+            toClient2.println(client1Port); 
+
+            toClient1.close(); 
+            toClient2.close(); 
+            client1.close(); 
+            client2.close(); 
+            
+        } catch (IOException e) {
+            System.err.println("Error Getting Input: " + e.getMessage());
+            System.exit(1); 
+        }
+
+
+    }
+}
