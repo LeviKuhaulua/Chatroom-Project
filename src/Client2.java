322f53a642cbba7c8b7580018453df2581ff7e7b
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-04-23 23:45:57 -1000

    Modified client2 java file

--- src/Client2.java
+++ src/Client2.java
@@ -9,8 +9,10 @@ import java.net.UnknownHostException;
  * @author
  *  Levi Kuhaulua
  */
-public class Client2 {
-    public static void main(String[] args) throws IOException{
+public class Client2 extends Thread {
+    private static boolean bye = false; 
+    
+    public static void main(String[] args) throws IOException {
         String client1IP = null; 
         int client1Port = 12345;  
         
@@ -18,11 +20,11 @@ public class Client2 {
         ServerSocket connSocket = new ServerSocket(23456); 
 
         // Communicate with Controller Socket to get IP and Port of other client. Replace hostname with target IP. 
-        try (Socket controllerClient = new Socket("127.0.0.1", 12345)) {
+        try (Socket controllerClient = new Socket("192.168.0.8", 12345)) {
             
             // Send out it's own information to Controller client. 
             PrintWriter toController = new PrintWriter(controllerClient.getOutputStream(), true); 
-            toController.println(connSocket.getInetAddress().getHostAddress()); 
+            toController.println(controllerClient.getLocalAddress().getHostAddress()); 
             toController.println(connSocket.getLocalPort()); 
 
             BufferedReader fromController = new BufferedReader(new InputStreamReader(controllerClient.getInputStream())); 
@@ -46,33 +48,52 @@ public class Client2 {
 
         // Communicate with the other client. Need to debug / test. 
         try (
-             Socket client1 = connSocket.accept();
+             Socket connClient1 = new Socket(client1IP, client1Port); 
+             Socket client1 = connSocket.accept(); 
              BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
         ) {
-
-            String socketLine = null; 
+ 
             String inputLine = null; 
-            BufferedReader fromClient1 = new BufferedReader(new InputStreamReader(client1.getInputStream())); 
+            BufferedReader fromClient1 = new BufferedReader(new InputStreamReader(connClient1.getInputStream())); 
             PrintWriter toClient1 = new PrintWriter(client1.getOutputStream(), true); 
 
-            // Continously communicate until client (us) disconnects. 
-            while ((socketLine = fromClient1.readLine()) != null) {
-                System.out.println("Client 2: " + socketLine);
-                while ((inputLine = stdIn.readLine()) != null) {
-                    if (inputLine.trim().equalsIgnoreCase("bye")) {
-                        toClient1.println(inputLine); 
-                        break; 
-                    } else {
-                        toClient1.println(inputLine);
+            
+            // Handle reading the messages. 
+            Thread readMsg = new Thread(() -> {
+                String socketLine = null; 
+                try {
+                    while ((socketLine = fromClient1.readLine()) != null) {
+                        if (socketLine.equalsIgnoreCase("Bye")) {
+                            System.out.println("Client is leaving the chat.");
+                            bye = true; 
+                            break;
+                        } else {
+                            System.out.println("Client 1: " + socketLine);
+                        }
                     }
+                } catch (IOException e) {
+                    System.out.println("Error reading input. " + e.getMessage());
+                }
+            });
+            
+            // Start the process of reading and sending out messages. 
+            readMsg.start(); 
+            while ((inputLine = stdIn.readLine()) != null && !bye) {
+                if (inputLine.trim().equalsIgnoreCase("Bye")) {
+                    toClient1.println(inputLine);
+                    break; 
+                } else {
+                    toClient1.println(inputLine);
                 }
             }
+            
 
             // Close connections. 
             fromClient1.close(); 
             toClient1.close(); 
             stdIn.close(); 
             client1.close(); 
+            connClient1.close(); 
         } catch (UnknownHostException e) {
             System.err.println("Unknown Host: " + e.getMessage());
             System.exit(-1); 
